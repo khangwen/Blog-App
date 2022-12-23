15c75069df2dca6dbc1ffefe03df5c830b5227a4
khangwen <a_wen197@yahoo.com>
2022-12-22 20:44:14 -0800

    cmplted (1 key warning, unordered->need timestamp)

--- blog-name/src/firebase-config.js
+++ blog-name/src/firebase-config.js
@@ -7,12 +7,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBitIDAR4qxt4ZfobxnsJgq7FRduqJmzh4",
-    authDomain: "blog-app-a88f3.firebaseapp.com",
-    projectId: "blog-app-a88f3",
-    storageBucket: "blog-app-a88f3.appspot.com",
-    messagingSenderId: "936995029413",
-    appId: "1:936995029413:web:70fbcc28e799871a6aa84c"
+    apiKey: "AIzaSyCZTW-O9fcAVHbjQipe0wfF_LUsQ3HuFbQ",
+    authDomain: "blog-app-2-e83e4.firebaseapp.com",
+    projectId: "blog-app-2-e83e4",
+    storageBucket: "blog-app-2-e83e4.appspot.com",
+    messagingSenderId: "306816517650",
+    appId: "1:306816517650:web:5d019e51ce896194af3acb"
 };
 
 // Initialize Firebase
